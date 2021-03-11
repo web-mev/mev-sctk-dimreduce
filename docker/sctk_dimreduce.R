4ce91dec9762c403b4b90223854701a0862ae099
dkdeconti <ddeconti@hsph.harvard.edu>
2021-03-11 13:58:14 -0500

    Changed input from SCE import to TSV import.

--- docker/sctk_dimreduce.R
+++ docker/sctk_dimreduce.R
@@ -10,19 +10,49 @@ suppressMessages(suppressWarnings(library(singleCellTK)))
 #     features.tsv
 #     matrix.mtx
 # ex: ./data/5k_pbmc/outs/filtered_feature_bc_matrix
-sce <- importCellRanger(
-    cellRangerDirs = "./data",
-    sampleDirs = "5k_pbmc",
-    sampleNames = "5k_pbmc",
-    dataType = "filtered",
-    gzipped = "auto"
+#sce <- importCellRanger(
+#    cellRangerDirs = "./data",
+#    sampleDirs = "5k_pbmc",
+#    sampleNames = "5k_pbmc",
+#    dataType = "filtered",
+#    gzipped = "auto"
+#)
+
+
+# args from command line:
+args <- commandArgs(TRUE)
+RAW_COUNT_MATRIX <- args[1]
+OUTPUT_UMAP_BASE <- 'umap_matrix'
+
+# Import counts as a 
+counts <- read.table(
+    file = RAW_COUNT_MATRIX,
+    sep = "\t",
+    row.names = 1
 )
 
-# Adding UMAP data to the SCE object
+# Create an SCE object from the counts
+sce <- SingleCellExperiment(
+    assays=list(counts=counts)
+)
+
+# PCA as a pre-processing step prior to UMAP dimensionality reduction.
+# Important to decorrelate the counts prior to UMAP.
+# Also log normalizes the counts.
+# Default to 50 PCA dims. Higher than typically needed.
+# Performing UMAP (on PCA) and adding UMAP data to the SCE object
 sce <- getUMAP(
     inSCE = sce, 
     useAssay = "counts", 
-    reducedDimName = "UMAP"
+    reducedDimName = "UMAP",
+    logNorm = TRUE,
+    nNeighbors = 30, 
+    nIterations = 200, 
+    alpha = 1,
+    minDist = 0.01,
+    pca = TRUE,
+    initialDims = 50
 )
 
+# Export UMAP values to file
 df.umap <- SingleCellExperiment::reducedDim(sce, "UMAP")
